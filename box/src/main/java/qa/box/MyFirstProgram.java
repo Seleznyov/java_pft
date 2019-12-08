d78935a41914b867418bb4351c5d281464c2157c
Seleznyov <58526725+Seleznyov@users.noreply.github.com>
2019-12-08 21:19:46 +0300

    Проект отвязан от репозитория Git

--- box/src/main/java/qa/box/MyFirstProgram.java
+++ box/src/main/java/qa/box/MyFirstProgram.java
@@ -1,10 +1,9 @@
 package qa.box;
 
-public class MyFirstProgram
-{
-	
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
-	}
-} 
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
+
 
